16210be02374cc57cf876ae35bfef9021ebcb39b
Umar Farooq <umar.wzbd@gmail.com>
2023-03-05 17:13:15 +0500

    Special Template Features & Text

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,7 +1,7 @@
 from datetime import date
 from django.shortcuts import render
 
-posts_data = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -62,8 +62,16 @@ posts_data = [
 ]
 
 
+def get_date(post):
+    return post.get('date')
+
+
 def index(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 
 def posts(request):
