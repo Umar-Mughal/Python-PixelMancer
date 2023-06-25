d3f0269207a814d85c2f911c01a3940e3afec05e
Umar Farooq <umar.wzbd@gmail.com>
2023-06-25 13:52:31 +0500

    save

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-# Create your models here.
 class Book(models.Model):
     # id = models.AutoField() # it sets automatically by django
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_best_selling = models.BooleanField(default=False)
